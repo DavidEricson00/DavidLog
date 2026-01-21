dae2db3c86e23f6056a6e897060aefc22e314dd2
David Ericson <davidestudofcg@gmail.com>
2026-01-21 18:42:44 -0300

    feat: list and sort posts in index.php
    Updated index.php to dynamically list HTML posts from the posts directory and sort them.

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
-    echo "David Log"
+$baseDir = __DIR__ . "/posts";
+
+$posts = [];
+
+foreach (glob("$baseDir/*/*/*/*.html") as $file){
+    $lines = file($file, FILE_IGNORE_NEW_LINES);    
+    $title = array_shift($lines);
+
+    $posts[] = [
+        "title" => $title
+        "path" => str_replace($baseDir . "/", "", $file)
+    ]
+}
+
+usort($posts, function ($a, $b, $c){
+    return strcmp($c["path"], $b["path"], $a["path"])
+});
 ?>
